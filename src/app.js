52a79ed94f94ac528ff19988a8a359f914acb872
pabloftc <pabloftc@gmail.com>
2022-01-12 22:47:38 +0000

    Ejercicio Excuse Generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Elige aleatoriamente los elementos del array
+  function aleatorio(newArray) {
+    let x = Math.floor(Math.random() * newArray.length);
+    return newArray[x];
+  }
+  // Concatena los elementos de los arrays
+  function excusa(arreglo) {
+    let frase = "";
+    arreglo.forEach(element => {
+      frase += aleatorio(element) + " ";
+    });
+    return frase;
+  }
+  // Inserta la frase en el HTML
+  function insertarElemento(elemento, identificador, texto) {
+    let parrafo = document.createElement(elemento);
+    parrafo.innerHTML = texto;
+    document.getElementById(identificador).appendChild(parrafo);
+  }
+
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let textoAInsertar = excusa([who, action, what, when]);
+  insertarElemento("p", "container", textoAInsertar);
 };
